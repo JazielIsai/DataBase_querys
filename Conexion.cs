0a65ab6a7967c8834f807ed32b59face7bd946d3
Jaziel Isaí <92839539+JazielIsai@users.noreply.github.com>
2022-05-29 18:48:08 -0500

    Add files via upload

---
+++ Conexion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public class Conexion
+{
+    //string StrCnx = "Integrated Security=false; Data Source=PCDUBI\\SQL2005; user=sa; password=1234; Initial Catalog=POO;";       // AUNTENTICACION USUARIO Y PASSWORD //  
+    string StrCnx = "Data Source = JASONPC; Initial Catalog = Proyecto; Integrated Security = True";                                // AUNTENTICACION DE WINDOWS SQL //
+
+    
+    public bool Ejecutar(string Query) 
+    {
+        SqlConnection ObjCnx = new SqlConnection(StrCnx);
+
+        try 
+        {
+            ObjCnx.Open();
+
+            SqlCommand ObjSqlCmd = new SqlCommand(Query, ObjCnx);
+            ObjSqlCmd.ExecuteNonQuery();
+
+            return true;
+        }
+        catch {
+
+            return false;
+        }
+    }
+
+
+    public DataTable RegresaValores(string Query)
+    {
+        SqlConnection ObjCnx = new SqlConnection(StrCnx);
+        DataSet Dtset = new DataSet();
+
+        try
+        {
+            ObjCnx.Open();
+
+            SqlCommand ObjSqlCmd = new SqlCommand(Query, ObjCnx);
+            ObjSqlCmd.ExecuteNonQuery();
+
+            SqlDataAdapter da = new SqlDataAdapter(ObjSqlCmd);
+            da.Fill(Dtset);
+
+            return Dtset.Tables[0];
+        }
+        catch
+        {
+            return null;
+        }
+    }
+  
+
+}
+
+
+
